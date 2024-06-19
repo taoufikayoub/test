99b8fce3251573e16027c24fb1623f886d3968e2
ayoub <ayoub@terros.io>
2024-06-19 19:46:32 +0100

    chore: Fix indentation in `fibonacci.py`

--- fibonacci.py
+++ fibonacci.py
@@ -8,7 +8,7 @@ def fibonacci(n):
     Returns:
     int: The nth Fibonacci number.
     """
-if n <= 1:
-        return n
+    if n <= 1:
+            return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
